549eb21a1973a51b8603b7e9145adc6c603aaa3c
m-a-huber <hubermarius@hotmail.com>
2025-05-06 23:22:54 +0200

    Restructure for evaluation

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def train_eval_svm(
     filtration_type: str,
     use_extended_persistence: bool,
     n_splits: int,
-    param_grid: dict,
+    search_space: dict,
     n_iter: int,
     n_jobs: int | None,
     verbose: int,
@@ -87,7 +87,7 @@ def train_eval_svm(
         )
         bayes_search = BayesSearchCV(
             estimator=pipeline,
-            search_spaces=param_grid,
+            search_spaces=search_space,
             n_iter=n_iter,
             scoring="roc_auc",
             n_jobs=n_jobs,
@@ -129,10 +129,7 @@ def train_eval_svm(
 
 
 if __name__ == "__main__":
-    filtration_type = sys.argv[1]  # must be one of "normal", "sloped",
-    # "sigmoidal", and "arctan"
-    use_extended_persistence = sys.argv[2] == "True"
-    overwrite = sys.argv[3] == "True"
+    overwrite = sys.argv[1] == "True"
 
     n_splits = 5  # number of splits in StratifiedKFold
     n_iter = 50  # number of iterations for BayesSearchCV
@@ -168,43 +165,82 @@ if __name__ == "__main__":
     # Set parameter space for BayesSearchCV; best params found by hand are [
     #     -0.25, 0.1, 1, "auto", "linear"
     # ] (in the order below)
-    param_grid = {
-        "time_series_homology__sigmoid_slope": Real(-1, 1, prior="uniform"),
-        "persistence_imager__base_estimator__bandwidth": Real(
-            0.01, 0.1,
-            prior="log-uniform"
-        ),
-        "svc__C": Real(0.1, 10, prior="log-uniform"),
-        "svc__gamma": Real(0.01, 10, prior='log-uniform'),
-        "svc__kernel": Categorical(["linear", "rbf"]),
-    }
-    suffix = (
-        "with_extended_persistence"
-        if use_extended_persistence
-        else "without_extended_persistence"
-    )
-    out_dir = Path(
-        f"out_files_{filtration_type}_{suffix}"
-    )
-    cv_results, best_params, cv_roc_scores = (
-        train_eval_svm(
-            X=X,
-            y=y,
-            out_dir=out_dir,
-            filtration_type=filtration_type,
-            use_extended_persistence=use_extended_persistence,
-            n_splits=n_splits,
-            param_grid=param_grid,
-            n_iter=n_iter,
-            n_jobs=n_jobs,
-            overwrite=overwrite,
-            verbose=verbose,
-            random_state=random_state,
-        )
-    )
-    print("Best Parameters:")
-    print(best_params)
-    print("Cross-validated ROC AUC scores:")
-    print(cv_roc_scores)
-    print("Average ROC AUC scores:")
-    print(f"{cv_roc_scores.mean()} ± {cv_roc_scores.std()}")
+    for filtration_type in ["normal", "sloped", "sigmoidal", "arctan"]:
+        if filtration_type == "normal":
+            search_space = {
+                "persistence_imager__base_estimator__bandwidth": Real(
+                    0.01, 0.1, prior="log-uniform"
+                ),
+                "svc__C": Real(0.1, 10, prior="log-uniform"),
+                "svc__gamma": Real(0.01, 10, prior="log-uniform"),
+                "svc__kernel": Categorical(["linear", "rbf"]),
+            }
+        if filtration_type == "sloped":
+            search_space = {
+                "time_series_homology__linear_slope": Real(
+                    -1, 1, prior="uniform"
+                ),
+                "persistence_imager__base_estimator__bandwidth": Real(
+                    0.01, 0.1, prior="log-uniform"
+                ),
+                "svc__C": Real(0.1, 10, prior="log-uniform"),
+                "svc__gamma": Real(0.01, 10, prior="log-uniform"),
+                "svc__kernel": Categorical(["linear", "rbf"]),
+            }
+        if filtration_type == "sigmoidal":
+            search_space = {
+                "time_series_homology__sigmoid_slope": Real(
+                    -1, 1, prior="uniform"
+                ),
+                "persistence_imager__base_estimator__bandwidth": Real(
+                    0.01, 0.1, prior="log-uniform"
+                ),
+                "svc__C": Real(0.1, 10, prior="log-uniform"),
+                "svc__gamma": Real(0.01, 10, prior="log-uniform"),
+                "svc__kernel": Categorical(["linear", "rbf"]),
+            }
+        if filtration_type == "arctan":
+            search_space = {
+                "time_series_homology__arctan_slope": Real(
+                    -1, 1, prior="uniform"
+                ),
+                "persistence_imager__base_estimator__bandwidth": Real(
+                    0.01, 0.1, prior="log-uniform"
+                ),
+                "svc__C": Real(0.1, 10, prior="log-uniform"),
+                "svc__gamma": Real(0.01, 10, prior="log-uniform"),
+                "svc__kernel": Categorical(["linear", "rbf"]),
+            }
+        for use_extended_persistence in [True, False]:
+            suffix = (
+                "with_extended_persistence"
+                if use_extended_persistence
+                else "without_extended_persistence"
+            )
+            out_dir = Path(
+                f"out_files_{filtration_type}_{suffix}"
+            )
+            print(f"Running {filtration_type}_{suffix}...")
+            cv_results, best_params, cv_roc_scores = (
+                train_eval_svm(
+                    X=X,
+                    y=y,
+                    out_dir=out_dir,
+                    filtration_type=filtration_type,
+                    use_extended_persistence=use_extended_persistence,
+                    n_splits=n_splits,
+                    search_space=search_space,
+                    n_iter=n_iter,
+                    n_jobs=n_jobs,
+                    overwrite=overwrite,
+                    verbose=verbose,
+                    random_state=random_state,
+                )
+            )
+            print("Best Parameters:")
+            print(best_params)
+            print("Cross-validated ROC AUC scores:")
+            print(cv_roc_scores)
+            print("Average ROC AUC scores:")
+            print(f"{cv_roc_scores.mean()} ± {cv_roc_scores.std()}")
+            print(f"Finished {filtration_type}_{suffix}.")
