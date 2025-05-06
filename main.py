87290da8dfc9eb9413a2ab4afd397105b9b62031
m-a-huber <hubermarius@hotmail.com>
2025-05-06 18:04:39 +0200

    Implement BayesSearchCV

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path  # type: ignore
 
 import gudhi.representations as gdrep  # type: ignore
@@ -10,13 +11,14 @@ from sklearn.metrics import (  # type: ignore
     roc_auc_score,
 )
 from sklearn.model_selection import (  # type: ignore
-    GridSearchCV,
     StratifiedKFold,
     train_test_split,
 )
 from sklearn.pipeline import Pipeline  # type: ignore
 from sklearn.preprocessing import MinMaxScaler, StandardScaler  # type: ignore
 from sklearn.svm import SVC  # type: ignore
+from skopt import BayesSearchCV  # type: ignore
+from skopt.space import Categorical, Real  # type: ignore
 
 from scripts.process_data import (  # type: ignore
     get_labels,
@@ -60,22 +62,26 @@ def train_eval_svm(
     X_test: npt.NDArray,
     y_test: npt.NDArray,
     out_dir: Path,
+    filtration_type: str,
     use_extended_persistence: bool,
     n_splits: int,
     param_grid: dict,
+    n_iter: int,
     n_jobs: int | None,
-    overwrite: bool,
     verbose: int,
+    overwrite: bool,
     random_state: int | None,
 ) -> tuple[dict, dict, dict, npt.NDArray, float]:
     if not out_dir.is_dir() or overwrite:
         TimeSeriesScaler = ListTransformer(base_estimator=StandardScaler())
         PersistenceImager = ListTransformer(gdrep.PersistenceImage(
+            resolution=(75, 75),
             weight=weight_abs1p
         ))
         pipeline = Pipeline([
             ("time_series_scaler", TimeSeriesScaler),
             ("time_series_homology", TimeSeriesHomology(
+                filtration_type=filtration_type,
                 use_extended_persistence=use_extended_persistence,
                 drop_infinite_persistence=not use_extended_persistence,
             )),
@@ -91,20 +97,22 @@ def train_eval_svm(
             shuffle=True,
             random_state=random_state,
         )
-        grid_search = GridSearchCV(
+        bayes_search = BayesSearchCV(
             estimator=pipeline,
-            param_grid=param_grid,
+            search_spaces=param_grid,
+            n_iter=n_iter,
             scoring="roc_auc",
             n_jobs=n_jobs,
             cv=cv,
             verbose=verbose,
+            random_state=random_state,
         )
-        grid_search.fit(X_train, y_train)
-        best_model = grid_search.best_estimator_
+        bayes_search.fit(X_train, y_train)
+        best_model = bayes_search.best_estimator_
         y_pred = best_model.predict(X_test)
         cv_results, best_params, clf_report, conf_matrix, roc_score = (
-            grid_search.cv_results_,
-            grid_search.best_params_,
+            bayes_search.cv_results_,
+            bayes_search.best_params_,
             classification_report(y_test, y_pred, output_dict=True),
             confusion_matrix(y_test, y_pred),
             roc_auc_score(y_test, best_model.decision_function(X_test)),
@@ -135,6 +143,16 @@ def train_eval_svm(
 
 
 if __name__ == "__main__":
+    filtration_type = sys.argv[1]
+    use_extended_persistence = sys.argv[2] == "True"
+    overwrite = sys.argv[3] == "True"
+
+    n_splits = 5  # number of splits in StratifiedKFold
+    n_iter = 2  # number of iterations for BayesSearchCV
+    n_jobs = -1  # parallelism for BayesSearchCV
+    verbose = 2
+    random_state = 42
+
     # Process corpus files and create time series data
     fixation_reports_dir = Path("data/FixationReports")
     dataset_statistics_dir = Path("data/DatasetStatistics")
@@ -145,40 +163,44 @@ if __name__ == "__main__":
     process_fixation_reports(
         fixation_reports_dir=fixation_reports_dir,
         out_dir=time_series_dir,
-        overwrite=False,
+        verbose=bool(verbose),
+        overwrite=overwrite,
     )
     get_labels(
         participants_stats_path=participants_stats_path,
         out_dir=time_series_dir / "labels",
-        overwrite=False,
+        verbose=bool(verbose),
+        overwrite=overwrite,
     )
-    # Create and train pipeline on extracted time series data
-    out_dir = Path("out_files")
-    use_extended_persistence = False
-    n_jobs = -1
-    overwrite = False
-    verbose = 2
-    random_state = 42
 
+    # Load data
     X_train, X_test, y_train, y_test = get_data(
         time_series_dir=time_series_dir,
         test_size=0.2,
         random_state=random_state,
     )
-    # Best params found by hand are [
-    #     "sigmoidal", -0.25, (75, 75), 0.1, 1, "auto", "linear"
-    # ] (in the order below)
-    n_splits = 5
-    param_grid = {
-        "time_series_homology__type": ["sigmoidal"],
-        "time_series_homology__sigmoid_slope": [-0.125, -0.25, -0.375, -0.5],
-        "persistence_imager__base_estimator__resolution": [(75, 75)],
-        "persistence_imager__base_estimator__bandwidth": [0.01, 0.05, 0.1],
-        "svc__C": [0.1, 1, 10],
-        "svc__gamma": [0.01, 0.1, "scale", "auto"],
-        "svc__kernel": ["linear", "rbf"],
-    }
 
+    # Set parameter space for BayesSearchCV; best params found by hand are [
+    #     -0.25, 0.1, 1, "auto", "linear"
+    # ] (in the order below)
+    param_grid = {
+        "time_series_homology__sigmoid_slope": Real(-1, 1, prior="uniform"),
+        "persistence_imager__base_estimator__bandwidth": Real(
+            0.01, 0.1,
+            prior="log-uniform"
+        ),
+        "svc__C": Real(0.1, 10, prior="log-uniform"),
+        "svc__gamma": Real(0.01, 10, prior='log-uniform'),
+        "svc__kernel": Categorical(["linear", "rbf"]),
+    }
+    suffix = (
+        "with_extended_persistence"
+        if use_extended_persistence
+        else "without_extended_persistence"
+    )
+    out_dir = Path(
+        f"out_files_{filtration_type}_{suffix}"
+    )
     cv_results, best_params, clf_report, conf_matrix, roc_score = (
         train_eval_svm(
             X_train=X_train,
@@ -186,9 +208,11 @@ if __name__ == "__main__":
             X_test=X_test,
             y_test=y_test,
             out_dir=out_dir,
+            filtration_type=filtration_type,
             use_extended_persistence=use_extended_persistence,
             n_splits=n_splits,
             param_grid=param_grid,
+            n_iter=n_iter,
             n_jobs=n_jobs,
             overwrite=overwrite,
             verbose=verbose,
