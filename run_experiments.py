2bafe564914f3be7411c35f47f7550891ef6e28b
m-a-huber <mariushuber2@gmail.com>
2025-11-25 14:47:21 +0100

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -1,25 +1,33 @@
+# ruff: noqa: E501
 import subprocess
 
 n_splits = 10
 n_iter = 100
 n_jobs = 8
+verbose = 1
+seed = 42
 
 common_flag = (
-    f"--n-splits {n_splits} --n-iter {n_iter} --n-jobs {n_jobs} --verbose"
+    f"--n-splits {n_splits} --n-iter {n_iter} --n-jobs {n_jobs} --verbose {verbose} --seed {seed}"
 )
 
 flag_sets = (
-    f"--model-name baseline_bjornsdottir {common_flag}",
-    f"--model-name baseline_raatikainen_svc {common_flag}",
-    f"--model-name baseline_raatikainen_rf {common_flag}",
-    f"--model-name tda_experiment_horizontal_ordinary {common_flag}",
-    f"--model-name tda_experiment_sloped_ordinary {common_flag}",
-    f"--model-name tda_experiment_sigmoid_ordinary {common_flag}",
-    f"--model-name tda_experiment_horizontal_extended {common_flag}",
-    f"--model-name tda_experiment_sloped_extended {common_flag}",
-    f"--model-name tda_experiment_sigmoid_extended {common_flag}",
+    f"--model-name baseline_bjornsdottir --classifier svc {common_flag}",
+    f"--model-name baseline_raatikainen --classifier svc {common_flag}",
+    f"--model-name baseline_raatikainen --classifier rf {common_flag}",
+    f"--model-name tda_experiment --filtration-type horizontal --classifier svc {common_flag}",
+    f"--model-name tda_experiment --filtration-type sloped --classifier svc {common_flag}",
+    f"--model-name tda_experiment --filtration-type sigmoid --classifier svc {common_flag}",
+    f"--model-name tda_experiment --filtration-type horizontal --classifier svc --use-extended-persistence {common_flag}",
+    f"--model-name tda_experiment --filtration-type sloped --classifier svc --use-extended-persistence {common_flag}",
+    f"--model-name tda_experiment --filtration-type sigmoid --classifier svc --use-extended-persistence {common_flag}",
+    # f"--model-name tda_experiment --filtration-type horizontal --classifier rf {common_flag}",
+    # f"--model-name tda_experiment --filtration-type sloped --classifier rf {common_flag}",
+    # f"--model-name tda_experiment --filtration-type sigmoid --classifier rf {common_flag}",
+    # f"--model-name tda_experiment --filtration-type horizontal --classifier rf --use-extended-persistence {common_flag}",
+    # f"--model-name tda_experiment --filtration-type sloped --classifier rf --use-extended-persistence {common_flag}",
+    # f"--model-name tda_experiment --filtration-type sigmoid --classifier rf --use-extended-persistence {common_flag}",
 )
 
 for flag_set in flag_sets:
-    cmd = " ".join(["uv run -m scripts.experiment", flag_set])
-    subprocess.run(cmd, shell=True)
+    subprocess.run(f"uv run -m scripts.experiment {flag_set}", shell=True)
